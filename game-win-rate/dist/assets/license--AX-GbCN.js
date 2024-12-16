20f477f2c3ee86e91bf678232d8670b866272647
陳依琳 <im1010ioio@gmail.com>
2024-12-16 17:36:33 +0800

    Added achievement description text

---
+++ game-win-rate/dist/assets/license--AX-GbCN.js
@@ -0,0 +1,1 @@
+import{c as e}from"./vue.esm-bundler-HwoN211w.js";e({setup(){return{year:new Date().getFullYear()}}}).mount("#app");
--- game-win-rate/dist/assets/license-BvkuZcS3.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./vue.esm-bundler-DWcbULsi.js";e({setup(){return{year:new Date().getFullYear()}}}).mount("#app");
